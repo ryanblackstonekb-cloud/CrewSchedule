8913e3a0d28eae60380aa2109c1fd62f8955e3c6
ryanblackstonekb-cloud <ryanblackstonekb@gmail.com>
2026-09-23 20:49:46 -0400

    Fix navigation bar inset import

--- app/src/main/java/com/example/crewschedule/MainActivity.kt
+++ app/src/main/java/com/example/crewschedule/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
